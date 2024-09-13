99ba6a66ea0bbd393238cc9d684784b0733b9000
Dinith Maleesha <maleesha.live123@gmail.com>
2024-09-13 17:05:01 +0530

    Tutorial #9 - Todo App (part 1)

--- App.js
+++ App.js
@@ -1,38 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
+import { FlatList, StyleSheet, Text, View, } from 'react-native';
+import Header from './components/header';
 
 export default function App() {
-  const [people, setPeople] = useState([
-    { name: 'shaun', id: '1' },
-    { name: 'dinith', id: '2' },
-    { name: 'maleesha', id: '3' },
-    { name: 'mario', id: '4' },
-    { name: 'yoshi', id: '5' },
-    { name: 'john', id: '6' },
-    { name: 'price', id: '7' },
-    { name: 'doe', id: '8' },
-  ]);
-
-  const pressHandler = (id) => {
-    console.log(id);
-    setPeople((prevPeople) => {
-      return prevPeople.filter(person => person.id != id)
-    })
-  }
+  const [todos, setTodos] = useState([
+    { text: 'Buy a coffee', key: '1' },
+    { text: 'Create an app', key: '2' },
+    { text: 'Play on the switch', key: '3' }
+  ])
 
   return (
     <View style={styles.container}>
-      <FlatList
-        numColumns={(2)}
-        keyExtractor={(item) => item.id}
-        data={people}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => { pressHandler(item.id) }}>
-            <Text style={styles.item}>{item.name}</Text>
-          </TouchableOpacity>
-        )}
-      />
+      {/* header */}
+      <Header/>
+      <View style={styles.content}>
+        {/* to do form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({item}) => (
+              <Text>{item.text}</Text>
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -41,15 +32,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingTop: 40,
-    paddingHorizontal: 20,
   },
-  item: {
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: 'pink',
-    fontSize: 24,
-    marginHorizontal: 10,
-    marginTop: 24
+  content: {
+    padding: 40
+  },
+  list: {
+    marginTop: 20
   }
 });
