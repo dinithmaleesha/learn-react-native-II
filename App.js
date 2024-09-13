5a47eedbb36c0b824774d6dfc82370e997f029b4
Dinith Maleesha <maleesha.live123@gmail.com>
2024-09-13 17:39:53 +0530

    Tutorial #10 - Todo App (part 2)

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import { useState } from 'react';
 import { FlatList, StyleSheet, Text, View, } from 'react-native';
 import Header from './components/header';
+import TodoItems from './components/todoItem';
 
 export default function App() {
   const [todos, setTodos] = useState([
     { text: 'Buy a coffee', key: '1' },
     { text: 'Create an app', key: '2' },
-    { text: 'Play on the switch', key: '3' }
+    { text: 'Play on the switch', key: '3' },
+    { text: 'Learn React', key: '4' },
   ])
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key)
+    })
+  }
 
   return (
     <View style={styles.container}>
@@ -19,7 +26,7 @@ export default function App() {
           <FlatList
             data={todos}
             renderItem={({item}) => (
-              <Text>{item.text}</Text>
+              <TodoItems item={item} pressHandler={pressHandler}/>
             )}
           />
         </View>
