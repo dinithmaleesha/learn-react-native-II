ab9f63a09a1f04bb80f022585e5220af228c4f73
Dinith Maleesha <maleesha.live123@gmail.com>
2024-09-14 23:19:23 +0530

    Tutorial #12 - Alerts

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { FlatList, StyleSheet, Text, View, } from 'react-native';
+import { Alert, FlatList, StyleSheet, Text, View, } from 'react-native';
 import Header from './components/header';
 import TodoItems from './components/todoItem';
 import AddTodo from './components/addTodo';
@@ -20,12 +20,19 @@ export default function App() {
   }
 
   const submitHandler = (text) => {
-    setTodos((prevTodos) => {
-      return [
-        { text: text, key: Math.random().toString() },
-        ...prevTodos
-      ]
-    })
+    if (text.length > 3) {
+      setTodos((prevTodos) => {
+        return [
+          { text: text, key: Math.random().toString() },
+          ...prevTodos
+        ]
+      })
+    } else {
+      Alert.alert('OOPS!', 'Todos must be over 3 chars long', [{
+        text: 'Understood', onPress: () => console.log('alert closed')
+      }])
+    }
+
   }
 
   return (
